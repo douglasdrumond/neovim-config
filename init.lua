a57583d426fc5efe57e71932e06bf19996053536
Douglas Kayama <douglas@cafelinear.com>
2021-11-27 21:59:57 -0300

    Add package manager

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- Inspired by
+-- https://gist.github.com/benfrain/97f2b91087121b2d4ba0dcc4202d252f
+
+require("plugins")
+require("options")
+require("mappings")
---
+++ plugins.lua
@@ -0,0 +1,53 @@
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+function get_setup(name)
+  return string.format('require("setup/%s")', name)
+end
+
+-- Auto install plugins on save
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- Auto install Packer if not available
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+end
+vim.api.nvim_command("packadd packer.nvim")
+
+return require("packer").startup({
+  function(use)
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
+
+    -- Add other packages
+
+    -- End (Add other packages)
+
+    if packer_bootstrap then
+      require("packer").sync()
+    end
+  end,
+  config = {
+    display = {
+      open_fn = require("packer.util").float,
+    },
+    profile = {
+      enable = true,
+      threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
+  },
+})
+
