18041103aa7b95583a8975dd838d0276132b6943
Douglas Kayama <douglas@cafelinear.com>
2021-11-29 17:10:17 -0300

    Make Windows recognize text copied in WSL Neovim

--- init.lua
+++ init.lua
@@ -4,3 +4,19 @@
 require("plugins")
 require("options")
 require("mappings")
+
+-- Further settings
+
+-- Make Windows clipboard detect text copied from Neovim
+local command = "uname -r"
+local handle = io.popen(command)
+local os_name = handle:read("*a")
+handle:close()
+if string.match(os_name, "microsoft") then
+  vim.cmd([[
+        augroup Yank
+            autocmd!
+            autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
+        augroup END
+    ]])
+end
